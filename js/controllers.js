1aa516cb504686ef06a13324c534d2cdb11a317b
BjarkeJ <bjarkeduejensen@gmail.com>
2015-10-24 13:35:40 +0200

    Push

--- js/controllers.js
+++ js/controllers.js
@@ -3,8 +3,16 @@ app.controller("ctrl", function($scope, benchmarkService) {
 	$scope.getBenchmark = function () {
 
     $scope.loading = true;
+    $scope.err = false;
+    $scope.boxShow = {
+      grossProfit: 'none',
+      employeeRatio: 'none',
+      roi: 'none',
+      currentRatio: 'none',
+    };
 
-		benchmarkService.get({ cvrNr: $scope.cvrNr }, function(benchmark) {
+		benchmarkService.get({ cvrNr: $scope.cvrNr }, function(err, benchmark) {
+        if(err) $scope.err = true;
 	    	$scope.benchmark = benchmark;
         $scope.loading = false;
 	  	});
