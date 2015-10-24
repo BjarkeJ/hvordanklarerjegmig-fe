782241b00bc806e5df634e7fcce223bc63933dfd
BjarkeJ <bjarkeduejensen@gmail.com>
2015-10-24 12:53:46 +0200

    progress

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 var app = angular.module('benchmark', ['ui.router', 'ngResource']);
+
+
+app.filter('percentage', ['$filter', function ($filter) {
+  return function (input, decimals) {
+    return $filter('number')(input * 100, decimals) + '%';
+  };
+}]);
--- js/services.js
+++ js/services.js
@@ -1,16 +1,90 @@
-app.factory('benchmarkService', function($resource) {
+app.factory('benchmarkService', function($resource, $http) {
 
+	// return {
+	// 	get: function(cvr) {
+	// 		console.log('requesting')
+	// 		var req = $http({
+	// 			method: 'GET',
+	// 			url: 'http://ec2-52-28-110-186.eu-central-1.compute.amazonaws.com:4299/metrics/' + cvr + '?industry=true&area=true&employees=true'
+	// 		}).then(function successCallback(res) {
+	// 	    // this callback will be called asynchronously
+	// 	    // when the response is available
+	// 	    console.log('Response:');
+	// 	    console.log(res);
+	// 	  }, function errorCallback(res) {
+	// 	    // called asynchronously if an error occurs
+	// 	    // or server returns response with an error status.
+	// 	    console.log('Response:');
+	// 	    console.log(res);
+	// 	  });
 
-	// return $resource('/');
+	// 	}
+	// };
 
 	return {
 		get: function(params, callback) {
 
-			var benchmark = {
-				nogletal1: 123,
-				nogletal2: 234,
-				nogletal3: 345,
-				nogletal4: 567
+			var params = {
+				branche: true,
+				omraade: true,
+				antalansatte: true,
+			};
+
+			benchmark = {
+			  name: 'Hackathon A/S',
+			  cvr: 12345678,
+			  fiscalYear: '2015',
+			  facets: {
+			    industry: 'Tandlæger',
+			    employees: '1-5',
+			    area: 'Sjælland',
+			  },
+			  metrics: {
+            grossProfit: {
+                own: 1234567,
+                median: 1999999,
+                rank: 1,
+                size: 1000,
+                rating: 1
+            },
+            resultBeforeTax: {
+                own: 1234567,
+                median: 1999999,
+                rank: 1,
+                size: 1000,
+                rating: 1
+            },
+            roi: {
+                own: 2.7,
+                median: 1.9,
+                rank: 1,
+                size: 1000,
+                rating: 1
+            },
+            currentRatio: {
+                own: 2.7,
+                median: 1.9,
+                rank: 1,
+                size: 1000,
+                rating: 1
+            },
+            employeeRatio: {
+                own: 2.7,
+                median: 1.9,
+                rank: 1,
+                size: 1000,
+                rating: 1
+            },
+        },
+			  competitors: [
+					'Virksomhed 1',
+					'Virksomhed 2',
+					'Virksomhed 3',
+					'Virksomhed 4',
+					'Virksomhed 5',
+					'Virksomhed 6',
+					'Virksomhed 7'
+			  ]
 			};
 
 			return callback(benchmark);
